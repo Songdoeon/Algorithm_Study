0b2742e0f3502d81eb94c6cdf944e8504ecdfd8f
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-07-03 21:47:21 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.91 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<survey.length;i++){
+            if(choices[i]<4){
+                char arg = survey[i].charAt(0);    
+                map.put(arg,map.getOrDefault(arg,0)+(4-choices[i]));
+            }
+            else {
+                char arg = survey[i].charAt(1);    
+                map.put(arg,map.getOrDefault(arg,0)+(choices[i]-4));
+            }
+        }
+
+        return new StringBuilder()
+            .append(map.getOrDefault('R', 0) >= map.getOrDefault('T', 0) ? 'R' : 'T')
+            .append(map.getOrDefault('C', 0) >= map.getOrDefault('F', 0) ? 'C' : 'F')
+            .append(map.getOrDefault('J', 0) >= map.getOrDefault('M', 0) ? 'J' : 'M')
+            .append(map.getOrDefault('A', 0) >= map.getOrDefault('N', 0) ? 'A' : 'N')
+            .toString();
+    }
+}
