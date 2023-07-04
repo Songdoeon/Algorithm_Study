06d285c6e5efbc1335ec6cfa6a1792af8b3dda66
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-07-04 11:06:51 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 1.92 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,17 +1,11 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        
-        for(int i=1;i<food.length;i++){
-            for(int j=0;j<food[i]/2;j++){
-                answer += Integer.toString(i);
-            }
+        StringBuilder builder = new StringBuilder();
+        for (int i=1; i<food.length; i++) {
+            int result = food[i] / 2;
+            builder.append(String.valueOf(i).repeat(result));
         }
-        answer += "0";
-        for(int i=answer.length()-2;i>=0;i--){
-            answer += answer.charAt(i);
-        }
-
-        return answer;
+        String answer = builder + "0";
+        return answer + builder.reverse();
     }
 }
