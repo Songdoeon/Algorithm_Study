41063fc7644b8a78b79a94514c745a042d1501f1
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-07-07 20:19:53 +0900

    [level 3] Title: 여행경로, Time: 11.01 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    
+    String[][] tickets;
+    ArrayList<String[]> answer = new ArrayList<>(); 
+    
+    void dfs(String route,int count,int[] visited){
+        String[] args = route.split(" ");
+        if(count==tickets.length){
+            answer.add(route.split(" "));
+            return ;
+        }
+        for(int i=0;i<tickets.length;i++){
+            if(tickets[i][0].equals(args[args.length-1]) && visited[i] == 0){
+                int[] newVisited = visited;
+                visited[i] = 1;
+                dfs(route + " " + tickets[i][1],count+1,newVisited);
+                newVisited[i] = 0;
+            }
+        }
+    }
+    public String[] solution(String[][] tickets) {
+        Arrays.sort(tickets, (o1, o2) -> o1[1].compareTo(o2[1]));
+        for(String[] arg : tickets){
+            System.out.println(arg[0]+ " " + arg[1]);
+        }
+        this.tickets = tickets;
+        int[] visited = new int[tickets.length];
+        dfs("ICN",0,visited);
+        return answer.get(0);
+    }
+}
