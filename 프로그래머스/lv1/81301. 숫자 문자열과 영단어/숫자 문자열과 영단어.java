6405ed61743c934800e2edeb75711ba1386b6ee3
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-07-14 20:18:14 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -9,15 +9,12 @@ class Solution {
         for(int idx=0;idx<numEng.length;idx++){
             map.put(numEng[idx],idx);
         }
+        
         for(int idx=0;idx<length-2;idx++){
             for(Map.Entry<String,Integer> entry : map.entrySet()){
                 // System.out.println(entry.getValue());
                 if(s.startsWith(entry.getKey(),idx)){
                     int wordLength = String.valueOf(entry.getKey()).length();
-                    // System.out.println(s.substring(0,idx));
-                    // System.out.println(entry.getValue());
-                    // System.out.println(s.substring(idx+wordLength));
-                    // System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
                     s = s.substring(0,idx)+entry.getValue()+s.substring(idx+wordLength);
                 }    
             }
