c41ae15773b6771b521a56f6a69c48bced85efa5
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-07-25 16:23:46 +0900

    [unrated] Title: 미로 탈출, Time: 0.49 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.java
@@ -2,58 +2,60 @@ import java.util.*;
 class Solution {
     int[] dX = new int[] {1,-1,0,0};
     int[] dY = new int[] {0,0,1,-1};
-    int cX;
-    int cY;
     int[] start;
-    int[] exit;
     int[] lever;
+    int[] exit;
     int answer = 0;
-    boolean light = false;
+    boolean s = false;
     public int solution(String[] maps) {
-        Character[][] map = new Character [maps.length][maps[0].length()];
-        int[][] visited = new int [maps.length][maps[0].length()];
+        int xLen = maps[0].length();
+        int yLen = maps.length;
+        Character[][] map = new Character[yLen][xLen];
+        int[][] visited = new int[yLen][xLen];
         Queue<int[]> point = new LinkedList<>();
         
-        for(int i=0;i<maps.length;i++){
-            for(int j=0;j<maps[i].length();j++){
-                switch(maps[i].charAt(j)){
-                    case 'S' : {start = new int[] {i,j};break;}
-                    case 'L' : {lever = new int[] {i,j};break;}
-                    case 'E' : {exit = new int[] {i,j};break;}
+        for(int idx = 0; idx < yLen;idx++){
+            for(int idx2 = 0; idx2 < xLen; idx2++){
+                Character c = maps[idx].charAt(idx2);
+                switch(c){
+                    case 'S' : {start = new int[] {idx,idx2};break;}
+                    case 'L' : {lever = new int[] {idx,idx2};break;}
+                    case 'E' : {exit = new int[] {idx,idx2};break;}
                 }
-                map[i][j]=maps[i].charAt(j);
+                map[idx][idx2] = c;
             }
         }
-        point.add(new int[] {start[0],start[1]});
+        point.add(start);
         
         while(!point.isEmpty()){
-            int[] p = point.poll();
-            
-            cX = p[0];
-            cY = p[1];
-            for(int i=0;i<4;i++){
-                int nX = cX+dX[i];
-                int nY = cY+dY[i];
-                if(nX<0 || nX >=maps.length || nY<0 || nY >=maps[0].length()){
-                    continue;
-                }
-                if(visited[nX][nY] == 0 && map[nX][nY] == 'L'){
+            int[] land = point.remove();
+            int cx = land[0];
+            int cy = land[1];
+            for(int i = 0; i < 4;i++){
+                int x = cx + dX[i];
+                int y = cy + dY[i];
+                
+                if(x < 0 || x >= yLen || y < 0 || y >= xLen) continue;
+                
+                if(visited[x][y] == 0 && map[x][y] == 'L'){ 
                     point.clear();
-                    point.add(new int[]{nX, nY});
-                    answer += visited[cX][cY];
+                    point.add(new int[] {x,y});
+                    answer += visited[cx][cy];
                     for(int j=0;j<visited.length;j++){
                         Arrays.fill(visited[j],0);
                     }
-                    visited[nX][nY]=1;
-                    light = true;
+                    visited[x][y]=1;
+                    s = true;
                     break;
                 }
-                if(visited[nX][nY] == 0 && map[nX][nY] !='X'){ 
-                    visited[nX][nY] = visited[cX][cY]+1;
-                    point.add(new int[] {nX,nY});
+                if(visited[x][y] == 0 && map[x][y] !='X'){ 
+                    visited[x][y] = visited[cx][cy]+1;
+                    point.add(new int[] {x,y});
                 }
-                if(map[nX][nY] == 'E' && light == true){
-                    return visited[nX][nY]+answer;
+                
+                if(map[x][y] == 'E' && s){
+                    System.out.println(visited[x][y]);
+                    return visited[x][y]+answer;
                 }
             }
         }
