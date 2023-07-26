21cec594f594622c9b64bbba9cd0433ace3bb11e
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-07-26 13:24:29 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length/n][n];
+        int length = num_list.length;
+        int idx = 0;
+        for(int Index = 0;Index < length;Index++){
+            if(Index == 0) answer[0][Index%3] = num_list[Index];
+            answer[Index/n][Index%n] = num_list[Index];
+        }
+        return answer;
+    }
+}
