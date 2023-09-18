eef3aadfebdd9579c0fe155945f94b51501e82be
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-09-18 16:34:36 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -3,29 +3,27 @@ class Solution {
     
     int computerNum;
     int answer;
-    int[][] computers;
-    void search(int node,boolean[] visited) {
+    void search(int node, int[][] network,boolean[] visited) {
         visited[node] = true;
-        
+
         for(int Index = 0; Index < computerNum;Index++){
-            if(visited[Index] == false && computers[node][Index] == 1){
-                search(Index, visited);
+            if(visited[Index] == false && network[node][Index] == 1){
+                search(Index, network, visited);
             }
         }
     }
-    
+
     public int solution(int n, int[][] computers) {
         computerNum = computers.length;
-        this.computers = computers;
         boolean[] visited = new boolean[computerNum];
-        
+        int[][] network = new int[computerNum][computerNum];
+
         for(int i = 0; i < computers.length; i++){
             if(visited[i] == false){
                 answer++;
-                search(i,visited);
+                search(i,computers,visited);
             }            
         }
-        
         return answer;
     }
 }
