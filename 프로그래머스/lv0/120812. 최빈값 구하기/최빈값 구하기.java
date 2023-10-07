483f965f2972d3e3e04ff1217e85556232f223e0
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-10-07 21:25:03 +0900

    [level 0] Title: 최빈값 구하기, Time: 1.84 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] array) {
+        if(array.length == 1) return array[0];
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < array.length; i++){
+            map.put(array[i], map.getOrDefault(array[i], 0) + 1);
+        }
+        
+        List<Integer> result = new ArrayList<>(map.values());
+        
+        int answer = result.stream().mapToInt(i -> i).max().getAsInt();
+        
+        int count = (int) result.stream().filter(i -> i == answer).count();
+        if(count != 1) return -1;
+        for(int i : map.keySet()){
+            if(map.get(i) == answer) return i;
+        }
+        return answer;
+    }
+}
