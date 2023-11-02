870b3c3df7af75afc3651a574f466b3173a82ae6
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-11-02 23:37:29 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean a = false;
+        boolean b = false;
+        if(x1 || x2) a = true;
+        if(x3 || x4) b = true;
+        if(a && b) return true;
+        return false;
+    }
+}
