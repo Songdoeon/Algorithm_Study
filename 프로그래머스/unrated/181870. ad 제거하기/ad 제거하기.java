a60c2770afe91362d30e39ae2fe2efc7697a0024
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-11-16 19:08:18 +0900

    [level unrated] Title: ad 제거하기, Time: 2.62 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -4,10 +4,7 @@ class Solution {
     public String[] solution(String[] strArr) {
         List<String> list = Arrays.stream(strArr).filter(s -> !s.contains("ad")).collect(toList());
         String[] answer = new String[list.size()];
-        int index = 0;
-        for(String str : list){
-            answer[index] = list.get(index++);
-        }
+        list.toArray(answer);
         return answer;
     }
 }
