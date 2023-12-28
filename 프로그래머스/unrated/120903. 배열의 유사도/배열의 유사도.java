b883c5f47d54afd641ebea2225f665e3fe6b48ed
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2023-12-28 15:18:15 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.07 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        Set<String> set1 = new HashSet<>(List.of(s1));
+        Set<String> set2 = new HashSet<>(List.of(s2));
+        set1.retainAll(set2);
+        return set1.size();
+    }
+}
