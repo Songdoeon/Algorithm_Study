7240f4a4bcfa7ed0b10387d53561dbacf67e691d
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-02-01 09:48:36 +0900

    [Gold III] Title: 줄 세우기, Time: 488 ms, Memory: 49676 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -7,34 +7,36 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		Queue<Integer> queue = new LinkedList<>();
 		StringTokenizer st = new StringTokenizer(br.readLine());
+        int n, n1, n2;
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		List<ArrayList<Integer>> list = new ArrayList<>();
-		int[] linked = new int[N + 1];
-		for(int i = 0; i <= N; i++) {
+		int[] linked = new int[N];
+		for(int i = 0; i < N; i++) {
 			list.add(new ArrayList<>());
 		}
 		for(int i = 0; i < M; i++) {
+			list.add(new ArrayList<>());
 			st = new StringTokenizer(br.readLine());
-			int n1 = Integer.parseInt(st.nextToken());
-			int n2 = Integer.parseInt(st.nextToken());
+			n1 = Integer.parseInt(st.nextToken()) - 1;
+			n2 = Integer.parseInt(st.nextToken()) - 1;
 			list.get(n1).add(n2);
 			linked[n2]++;
 		}
 		
-		for(int i = 1; i <= N; i++) {
+		for(int i = 0; i < N; i++) {
 			if(linked[i] == 0) {
 				queue.add(i);
 			}
 		}
 		while(!queue.isEmpty()) {
-			int n = queue.poll();
-			sb.append(n).append(" ");
+			n = queue.poll();
+			sb.append(n+1).append(" ");
 			for(int next : list.get(n)) {
 				if(--linked[next] == 0) {
-				    queue.add(next);
-			    }
-		    }
+				queue.add(next);
+				}
+			}
 		}
 		System.out.println(sb);
 		br.close();
