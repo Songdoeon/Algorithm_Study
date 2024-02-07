eb9f4b02dcc24a7027100f7797678e51d6a925ca
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-02-07 11:33:26 +0900

    [Silver II] Title: 최소 힙, Time: 284 ms, Memory: 25560 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -6,21 +6,22 @@ public class Main {
 	public static void main(String[] args) throws IOException {
  
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		Queue<Integer> queue = new PriorityQueue<>();
 		for(int i = 0; i < N; i++) {
 			int n = Integer.parseInt(br.readLine());
 			
 			if(n == 0 && !queue.isEmpty()) {
-				System.out.println(queue.poll());
+                sb.append(queue.poll()).append('\n');
 			}
 			else if(n == 0 && queue.isEmpty()) {
-				System.out.println("0");
+                sb.append("0").append('\n');
 			}
 			else {
 				queue.add(n);
 			}
-		}	
+		}
+        System.out.println(sb);
 	}
 }
