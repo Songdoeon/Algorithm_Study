20b3cf6fcfedbcbf97e43e57b8be2894235345fb
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-02-11 11:57:44 +0900

    [Gold IV] Title: 플로이드, Time: 360 ms, Memory: 43872 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    // 최대비용 100_000 * 최대 도시 100
+    static final int INF = 100_000_000;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = INF;
+
+                if (i == j) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            arr[start][end] = Math.min(arr[start][end], cost);
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (arr[i][j] == INF) {
+                    arr[i][j] = 0;
+                }
+                sb.append(arr[i][j]).append(' ');
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
