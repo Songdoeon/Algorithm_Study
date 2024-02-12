5ce727811a07b5206619f88b16ef793a3b60d813
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-02-12 15:42:12 +0900

    [Silver I] Title: 정수 삼각형, Time: 228 ms, Memory: 24136 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,9 +1,8 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
