c87076a0a1c4f66fce4dc5da6dbbdae20d306b1e
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-02-14 09:44:39 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 496 ms, Memory: 87336 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
@@ -4,22 +4,24 @@ import java.util.*;
 public class Main {
     static int N, S;
     static int[] arr;
-    static Map<Integer, Long> map = new HashMap<>();
+    static Map<Integer, Integer> map = new HashMap<>();
     static long answer = 0;
     static void search2(int depth, int end,int sum){
         if(depth == end) {
-            answer += map.getOrDefault(S - sum, 0L);
+            answer += map.getOrDefault(S - sum, 0);
             return;
         }
-        search2(depth + 1, end, sum + arr[depth]);
+        search2(depth + 1, end, sum);
+        sum += arr[depth];
         search2(depth + 1, end, sum);
     }
     static void search(int depth, int end, int sum){
         if(depth == end){
-            map.put(sum, map.getOrDefault(sum, 0L) + 1L);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
             return ;
         }
-        search(depth + 1, end,sum + arr[depth]);
+        search(depth + 1, end,sum);
+        sum += arr[depth];
         search(depth + 1, end, sum);
     }
     public static void main(String[] args) throws IOException {
@@ -28,7 +30,7 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
-        arr = new int[N + 1];
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
