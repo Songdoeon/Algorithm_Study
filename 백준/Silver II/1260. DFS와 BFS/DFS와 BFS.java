737d42b01bbcacb3947c668e4865055abc606054
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-02-16 12:00:30 +0900

    [Silver II] Title: DFS와 BFS, Time: 276 ms, Memory: 20256 KB -BaekjoonHub

--- 백준/Silver II/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver II/1260. DFS와 BFS/DFS와 BFS.java
@@ -3,18 +3,17 @@ import java.io.*;
 
 class Main{
     static StringBuilder sb = new StringBuilder();
-    static List<List<Integer>> graph = new ArrayList<>();
+    static List<Integer>[] graph;
     static boolean[] visited;
     static int N, M, V;
 
     static void search(int n){
-
-        for (int next : graph.get(n)){
-            if(visited[next]) continue;
+        visited[n] = true;
+        sb.append(n).append(' ');
+        for(int next : graph[n]){
+            if(visited[next])continue;
             visited[next] = true;
-            sb.append(next).append(' ');
             search(next);
-
         }
     }
     public static void main(String[] args) throws IOException{
@@ -25,21 +24,21 @@ class Main{
         M = Integer.parseInt(st.nextToken());
         V = Integer.parseInt(st.nextToken());
         visited = new boolean[N + 1];
-        for(int i = 0; i <= N; i++){
-            graph.add(new ArrayList<>());
+        graph = new List[N + 1];
+        for (int i = 0; i <= N; i++) { // 그래프 생성
+            graph[i] = new ArrayList<>();
         }
+
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            graph.get(end).add(start);
-            graph.get(start).add(end);
+            graph[start].add(end);
+            graph[end].add(start);
         }
         for(int i = 1; i <= N; i++){
-            graph.get(i).sort(Comparator.comparingInt(o -> o));
+            graph[i].sort(Comparator.comparing(o -> o));
         }
-
-        sb.append(V).append(' ');
         visited[V] = true;
         search(V);
         sb.append('\n');
@@ -47,15 +46,14 @@ class Main{
         Arrays.fill(visited, false);
         visited[V] = true;
         queue.add(V);
-        sb.append(V).append(' ');
+        int p;
         while(!queue.isEmpty()){
-            int p = queue.poll();
-
-            for(int next : graph.get(p)){
-                if(visited[next]) continue;
+            p = queue.poll();
+            sb.append(p).append(' ');
+            for(int next : graph[p]){
+                if(visited[next])continue;
                 visited[next] = true;
-                queue.add(next);
-                sb.append(next).append(' ');
+                queue.offer(next);
             }
         }
         System.out.println(sb);
