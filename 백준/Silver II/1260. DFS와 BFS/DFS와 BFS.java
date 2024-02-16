85c22e2893811542a78d670ac73b93feeee4022e
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-02-16 12:06:55 +0900

    [Silver II] Title: DFS와 BFS, Time: 176 ms, Memory: 17500 KB -BaekjoonHub

--- 백준/Silver II/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver II/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,79 +1,63 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static int N, M, V;
-    static boolean[] visited;
-    static ArrayList<Integer>[] graph;
+class Main{
     static StringBuilder sb = new StringBuilder();
+    static List<Integer>[] graph;
+    static boolean[] visited;
+    static int N, M, V;
 
-    public static void main(String[] args) throws IOException {
-        input();
-        pro();
+    static void search(int n){
+        visited[n] = true;
+        sb.append(n).append(' ');
+        List<Integer> childs = graph[n];
+        for(Integer child : childs){
+            if(visited[child])continue;
+            visited[child] = true;
+            search(child);
+        }
     }
-
-    static void input() throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        Queue<Integer> queue = new ArrayDeque<>();
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         V = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[N + 1];
-        for (int i = 0; i < graph.length; i++)
+        visited = new boolean[N + 1];
+        graph = new List[N + 1];
+        for (int i = 0; i <= N; i++) { // 그래프 생성
             graph[i] = new ArrayList<>();
-        for (int i = 1; i <= M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
         }
-        for(int i = 1; i <= N; i++) {
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            graph[start].add(end);
+            graph[end].add(start);
+        }
+        for(int i = 1; i <= N; i++){
             Collections.sort(graph[i]);
         }
-        visited = new boolean[N + 1];
-    }
-
-    static void pro() {
-        dfs(V);
-        sb.append("\n");
-        // visited 초기화
-        reset_visited();
-        bfs();
-
-        System.out.println(sb);
-    }
-
-    static void reset_visited() {
-        Arrays.fill(visited, false);
-    }
-
-    static void dfs(int start) {
-        sb.append(start).append(" ");
-        visited[start] = true;
-        for(int x : graph[start]) {
-            if(!visited[x]) {
-                dfs(x);
-            }
-        }
-    }
-
-    static void bfs() {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(V);
         visited[V] = true;
-        sb.append(V).append(" ");
+        search(V);
+        sb.append('\n');
 
-        while(!q.isEmpty()) {
-            int x = q.poll();
-
-            for(int nx : graph[x]) {
-                if(!visited[nx]) {
-                    visited[nx] = true;
-                    q.add(nx);
-                    sb.append(nx).append(" ");
-                }
+        Arrays.fill(visited, false);
+        visited[V] = true;
+        queue.add(V);
+        int p;
+        while(!queue.isEmpty()){
+            p = queue.poll();
+            sb.append(p).append(' ');
+            List<Integer> childs = graph[p];
+            for(Integer child : childs){
+                if(visited[child])continue;
+                visited[child] = true;
+                queue.offer(child);
             }
         }
+        System.out.println(sb);
     }
 }
