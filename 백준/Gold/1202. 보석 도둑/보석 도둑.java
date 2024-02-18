86aeaa583fb6a11e55c02ac1457a6aa1c8aab528
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-02-18 13:32:04 +0900

    [Gold II] Title: 보석 도둑, Time: 1212 ms, Memory: 126012 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -9,41 +9,38 @@ class Jewel{
         this.weight = w;
         this.price = p;
     }
-    
+
 }
 class Main{
     static PriorityQueue<Integer> jewelQueue = new PriorityQueue<>(Collections.reverseOrder());
-    static List<Jewel> jewelList = new ArrayList<>();
-    public static void main(String args[]) throws Exception{
+    static PriorityQueue<Jewel> jewelList = new PriorityQueue<>(Comparator.comparingInt(o -> o.weight));
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] arg = br.readLine().split(" ");
         int N = Integer.parseInt(arg[0]);
         int K = Integer.parseInt(arg[1]);
         long answer = 0;
         StringTokenizer st;
-
+        int weight, price;
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
-            int weight = Integer.parseInt(st.nextToken());
-            int price = Integer.parseInt(st.nextToken());
-            jewelList.add(new Jewel(weight, price));
+            weight = Integer.parseInt(st.nextToken());
+            price = Integer.parseInt(st.nextToken());
+            jewelList.offer(new Jewel(weight, price));
         }
-        jewelList.sort((o1, o2) -> o1.weight == o2.weight ? o2.price - o1.price : o1.weight - o2.weight);
         int[] backArr = new int[K];
         for(int i = 0; i < K; i++){
             backArr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(backArr);
-
-        int weightIdx = 0;
         for(int i = 0; i < K; i++){
-            while(weightIdx < N && jewelList.get(weightIdx).weight <= backArr[i]){
-                Jewel jewel = jewelList.get(weightIdx++);
+            while(!jewelList.isEmpty() && jewelList.peek().weight <= backArr[i]){
+                Jewel jewel = jewelList.poll();
                 jewelQueue.add(jewel.price);
             }
             if(!jewelQueue.isEmpty()) answer += jewelQueue.poll();
         }
-
         System.out.println(answer);
     }
 }
