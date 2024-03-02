a7ebf28bce54376d51514954cadda337620f3e54
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-03-02 14:39:31 +0900

    [Silver III] Title: 2×n 타일링, Time: 84 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if(N == 1) {
+            System.out.println(1);
+            return ;
+        }
+        final int MOD = 10_007;
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1]  + dp[i - 2]) % MOD;
+        }
+        System.out.println(dp[N]);
+    }
+}
