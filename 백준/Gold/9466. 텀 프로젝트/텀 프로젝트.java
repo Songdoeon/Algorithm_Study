47da2dffa9c4991246069db71e59e98ff4eea1f5
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-03-02 14:44:49 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1128 ms, Memory: 302484 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -2,19 +2,19 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    static BitSet visited, selected;
+    static boolean[] visited, selected;
     static int[] arr;
     static int ans;
     static void search(int idx){
-        if(selected.get(idx)) return ;
-        if(visited.get(idx)) {
-            selected.set(idx);
+        if(selected[idx]) return ;
+        if(visited[idx]) {
+            selected[idx] = true;
             ans++;
         }
-        visited.set(idx);
+        visited[idx] = true;
         search(arr[idx]);
-        selected.set(idx);
-        visited.flip(idx);
+        selected[idx] = true;
+        visited[idx] = false;
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,15 +27,15 @@ class Main{
 
             ans = 0;
             arr = new int[N + 1];
-            visited = new BitSet(N + 1);
-            selected = new BitSet(N + 1);
+            visited = new boolean[N + 1];
+            selected = new boolean[N + 1];
             st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= N; i++) {
                 int n = Integer.parseInt(st.nextToken());
                 arr[i] = n;
             }
             for (int i = 1; i <= N; i++) {
-                if(selected.get(i)) continue;
+                if(selected[i]) continue;
                 search(i);
             }
             sb.append(N - ans).append('\n');
