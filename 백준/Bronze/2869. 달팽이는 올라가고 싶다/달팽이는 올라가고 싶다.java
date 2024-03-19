f76092ba7c299c2136f619983f4e77b19d2af44d
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-03-19 22:05:56 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 116 ms, Memory: 12840 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -3,12 +3,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arg = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        int high = arg[0] - arg[1];
-        int floor = (arg[2] - arg[0]) / high;
-        if((arg[2] - arg[0]) % high != 0){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int N = sc.nextInt();
+        
+        int high = A - B;
+        int floor = (N - A) / high;
+        if((N - A) % high != 0){
             System.out.println(floor == 0 ? 2 : floor + 2);
         }
         else System.out.println(floor + 1);
