a77c2c7dc1f84adc51a9109b8951bd8bfaf23a00
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-03-24 13:37:39 +0900

    [Bronze V] Title: A+B - 2, Time: 112 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
