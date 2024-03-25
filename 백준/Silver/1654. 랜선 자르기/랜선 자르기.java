5b333a0704c1441bd2be15c4bf1513514fcd675a
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-03-25 20:35:02 +0900

    [Silver II] Title: 랜선 자르기, Time: 96 ms, Memory: 11968 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    static long[] arr;
+    static int[] arr;
     static int N, M;
 
     static long check(long mid){
@@ -12,16 +12,14 @@ class Main{
         }
         return n;
     }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception{
+        Reader in = new Reader();
+        N = in.nextInt();
+        M = in.nextInt();
         long sum = 0;
-        arr = new long[N];
-
+        arr = new int[N];
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[i] = in.nextInt();
             sum += arr[i];
         }
 
@@ -41,5 +39,32 @@ class Main{
             }
         }
         System.out.println(answer);
+    }
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            while ((c = read()) <= 32);
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0)
+                    buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
     }
 }
