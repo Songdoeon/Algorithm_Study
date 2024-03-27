1049c0b668123fe0a5bf5943b21aa8b561a3f1ab
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-03-27 21:30:51 +0900

    [Silver III] Title: 파도반 수열, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,26 +1,19 @@
 import java.io.*;
-import java.util.*;
 public class Main {
-
-    static int N, T;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        T = Integer.parseInt(br.readLine());
-        int[] t = new int[T];
-        for (int i = 0; i < T; i++) {
-            t[i] = Integer.parseInt(br.readLine());
-        }
+        int T = Integer.parseInt(br.readLine());
         long[] dp = new long[101];
         dp[1] = dp[2] = dp[3] = 1;
 
-        for (int i = 4; i <= 100; i++) {
+        for (int i = 4; i <= 100; i++) 
             dp[i] = dp[i - 2] + dp[i - 3];
-        }
-        for (int i = 0; i < T; i++) {
-            sb.append(dp[t[i]]).append('\n');
-        }
+        
+        for (int i = 0; i < T; i++) 
+            sb.append(dp[Integer.parseInt(br.readLine())]).append('\n');
+        
         System.out.println(sb);
     }
 }
