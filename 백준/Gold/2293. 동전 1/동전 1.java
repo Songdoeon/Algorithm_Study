971cdd5b4908874848d4c5f160558ff4009531bb
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-03-31 13:45:33 +0900

    [Gold V] Title: 동전 1, Time: 88 ms, Memory: 11780 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -4,8 +4,7 @@ import java.io.*;
 
 class Main{
     static int N, K;
-    static int[] arr;
-    static int[][] dp;
+    static int[] arr, dp;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,23 +14,16 @@ class Main{
         K = Integer.parseInt(st.nextToken());
 
         arr = new int[N + 1];
-        dp = new int[N + 1][K + 1];
+        dp = new int[K + 1];
+        dp[0] = 1;
 
         for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-        }
-        int sum = 0;
-        for (int j = 1; j <= K; j++) {
-            if(j % arr[1] == 0) dp[1][j]++;
-        }
-        dp[1][0] = 1;
-        for (int i = 2; i <= N; i++) {
-            for (int j = 0; j <= K; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if(j >= arr[i]) dp[i][j] += dp[i][j - arr[i]];
+            for (int j = arr[i]; j <= K; j++) {
+                dp[j] += dp[j - arr[i]];
             }
         }
 
-        System.out.println(dp[N][K]);
+        System.out.println(dp[K]);
     }
 }
