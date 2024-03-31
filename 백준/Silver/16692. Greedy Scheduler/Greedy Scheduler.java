3020299d3de8239ce10bd6593a706d998cc32498
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-03-31 18:00:21 +0900

    [Silver IV] Title: Greedy Scheduler, Time: 112 ms, Memory: 12060 KB -BaekjoonHub

--- 백준/Silver/16692. Greedy Scheduler/Greedy Scheduler.java
+++ 백준/Silver/16692. Greedy Scheduler/Greedy Scheduler.java
@@ -1,80 +1,38 @@
 import java.util.*;
 import java.io.*;
 
+public class Main {
 
-class Cashier{
-    int no;
-    PriorityQueue<Integer> queue;
-
-    public Cashier(int no) {
-        this.no = no;
-        queue = new PriorityQueue<>();
-    }
-}
-class Main{
-    static int N, K;
-    static PriorityQueue<Cashier> list = new PriorityQueue<>((o1, o2) -> {
-        if(!o1.queue.isEmpty()){
-            if(!o2.queue.isEmpty()){
-                if(Objects.equals(o1.queue.peek(), o2.queue.peek())){
-                    return o1.no - o2.no;
-                }
-                else return o1.queue.peek() - o2.queue.peek();
-            }
-
-            return 1;
-        }
-        if (!o2.queue.isEmpty()) {
-            return -1;
-        }
-        return o1.no - o2.no;
-    });
-    static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= N; i++) {
-            list.add(new Cashier(i));
-        }
+        int[] time = new int[N+1];
+        int[] arr = new int[C];
+
         st = new StringTokenizer(br.readLine());
-
-        int time = 0;
-        for (int i = 0; i < K; i++) {
-            // 시간
-            int n = Integer.parseInt(st.nextToken());
-
-            // 가장 빨리 끝날 계산대
-            Cashier c = list.poll();
-            sb.append(c.no).append(' ');
-
-            // 계산대 줄에 넣기
-            c.queue.offer(time + n);
-
-            // 큐에 다시 넣기
-            list.offer(c);
-
-
-//          전체 큐를 순회하며 현재 시간보다 적은 큐들을 비워준다.
-            for (int j = 0; j < N; j++) {
-                Cashier cashier = list.poll();
-                while (!cashier.queue.isEmpty()){
-                    if(cashier.queue.peek() > time) break;
-                    cashier.queue.poll();
-                }
-                list.offer(cashier);
-            }
-
-            // 다음 가장 빨리 끝날 계산대에서 다음 설정할 시간을 구한다.
-            if(!list.peek().queue.isEmpty()){
-                if(time < list.peek().queue.peek())
-                    time = list.peek().queue.peek();
-            }
+        for(int i = 0; i < C; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(sb);
+
+        int min;
+        for(int i = 0; i < C; i++) {
+            int idx = 1;
+            min = 1_000_001;
+            for(int j = 1; j < N + 1; j++) {
+                if(time[j] < min) {
+                    min = time[j];
+                    idx = j;
+                }
+            }
+            sb.append(idx).append(' ');
+            time[idx] += arr[i];
+        } 
+        System.out.print(sb);
     }
 }
