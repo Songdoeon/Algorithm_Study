92c167ac3e230ae85ee540d828bc9cc3baf9d311
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-04-05 09:42:54 +0900

    [Silver I] Title: IOIOI, Time: 188 ms, Memory: 20680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -3,39 +3,55 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.valueOf(br.readLine());
-        
-        int len = Integer.valueOf(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int len = Integer.parseInt(br.readLine());
         String str = br.readLine();
         int tempLen = 2 * N + 1;
 
-        StringBuilder sb = new StringBuilder("I");
         boolean button;
         int answer = 0;
-        for(int i = 0; i < N; i++){
-            sb.append("OI");
+        if(tempLen > len){
+            System.out.println(0);
+            return ;
         }
-        String temp = sb.toString();
         for(int i = 0; i <= len - tempLen; i++){
             char c = str.charAt(i);
             char nextC = str.charAt(i + 1);
             if(c == 'I' && nextC == 'O'){
+//                System.out.println(i + "들옴?");
                 int index = 2;
                 button = true;
-                for(int j = 0; j < tempLen - 2; j++){
-                    char tempC = str.charAt(j + i + 2);
-                    if(tempC == 'O' && !button){
-                        button = true;
-                        index++;
-                    } else if (tempC == 'I' && button) {
-                        button = false;
-                        index++;
+                for(int j = index; j < tempLen - 2; j += 2){
+                    char tempC = str.charAt(j + i);
+                    char tempNextC = str.charAt(j + i + 1);
+                    if(!(tempC == 'I') || !(tempNextC == 'O'))break;
+                    index += 2;
+                }
+                if(str.charAt(i + index) == 'I')index++;
+//                System.out.println(index);
+                if(index == tempLen) {
+                    answer++;
+                    int tempIdx = i + tempLen;
+//                    System.out.println(i + "nomal" + answer);
+                    if(tempIdx > len - 2) break;
+                    c = str.charAt(tempIdx);
+                    nextC = str.charAt(tempIdx + 1);
+                    while (c == 'O' && nextC == 'I'){
+                        answer++;
+//                        System.out.println(i + "??" + answer);
+                        i = tempIdx - 1;
+                        tempIdx += 2;
+                        if(tempIdx > len - 2) break;
+                        c = str.charAt(tempIdx);
+                        nextC = str.charAt(tempIdx + 1);
                     }
                 }
-                if(index == tempLen) answer++;
+                else{
+                    i += index - 1;
+                }
             }
         }
-
         System.out.println(answer);
     }
 }
