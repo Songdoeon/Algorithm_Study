003fd4f044841438a851415758ced0d5bce94ed7
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-04-10 17:48:34 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 84 ms, Memory: 11816 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int N, K;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10_001];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 3;
+        arr[4] = 4;
+        arr[5] = 5;
+        arr[6] = 7;
+        int idx = 1;
+        for (int i = 7; i <= 10_000; i++) {
+            if(i % 6 == 0) {
+                arr[i - 1]++;
+                idx++;
+            }
+            arr[i] = arr[i - 1] + idx;
+            if(i % 2 == 0 || i % 3 == 0)arr[i]++;
+        }
+        while (N-- > 0){
+            sb.append(arr[Integer.parseInt(br.readLine())]).append('\n');
+        }
+        System.out.println(sb.substring(0, sb.length()));
+    }
+}
