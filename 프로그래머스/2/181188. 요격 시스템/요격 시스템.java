227bdf866f918de10bb223011aa644fb3f82332c
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-04-15 10:34:53 +0900

    [level 2] Title: 요격 시스템, Time: 0.62 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -2,13 +2,12 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        
-        Arrays.sort(targets, (o1,o2) -> o1[1] - o2[1]);
-        int endTime = 0;
+        Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
+        int end = 0;
         for(int[] target : targets){
-            if(endTime <= target[0]){
+            if(end <= target[0]) {
                 answer++;
-                endTime = target[1];
+                end = target[1];
             }
         }
         return answer;
