fcdeafc103af91842150b710dd66714c88160187
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-04-18 12:11:47 +0900

    [Gold V] Title: 용액, Time: 280 ms, Memory: 31628 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,37 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
+    static int N;
+    static int[] arr;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N + 1];
-
+        N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for(int i = 0; i < N; i++){
+        arr = new int[N];
+        long result = 2_000_000_000;
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        int ldx = 0;
+        int rdx = N - 1;
         int start = 0;
         int end = N - 1;
-        int sum;
-        long answer = Long.MAX_VALUE;
-        int ans1 = 0;
-        int ans2 = 0;
-        while(start < end){
-            sum = arr[start] + arr[end];
-            if(answer >= Math.abs(sum)) {
-                answer = Math.abs(sum);
-                ans1 = start;
-                ans2 = end;
+        while (start < end){
+            long sum = arr[start] + arr[end];
+            if(sum == 0) {
+                ldx = start;
+                rdx = end;
+                break;
             }
-            if(sum >= 0) end--;
+            if(result > Math.abs(sum)){
+                ldx = start;
+                rdx = end;
+                result = Math.abs(sum);
+            }
+            if(sum > 0)end--;
             else start++;
         }
-        System.out.println(arr[ans1] + " " + arr[ans2]);
-    }
 
+        System.out.println(arr[ldx] + " " + arr[rdx]);
+    }
 }
