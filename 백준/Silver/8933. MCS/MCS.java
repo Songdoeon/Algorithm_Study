60f1408064bc478ae20df4717fd2e40153199589
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-04-21 20:12:45 +0900

    [Silver III] Title: MCS, Time: 1620 ms, Memory: 224396 KB -BaekjoonHub

---
+++ 백준/Silver/8933. MCS/MCS.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+class MCS{
+    int A, G, T, C;
+
+    public MCS(int a, int g, int t, int c) {
+        A = a;
+        G = g;
+        T = t;
+        C = c;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MCS mcs = (MCS) o;
+
+        if (A != mcs.A) return false;
+        if (G != mcs.G) return false;
+        if (T != mcs.T) return false;
+        return C == mcs.C;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = A;
+        result = 31 * result + G;
+        result = 31 * result + T;
+        result = 31 * result + C;
+        return result;
+    }
+}
+class Main{
+    static int N;
+    static int[] cntArr;
+    static Map<MCS, Integer> map;
+    static void add(){
+        MCS mcs = new MCS(cntArr[0], cntArr[6], cntArr[19], cntArr[2]);
+        map.put(mcs, map.getOrDefault(mcs, 0) + 1);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            cntArr = new int[26];
+            map = new HashMap<>();
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            String DNA = st.nextToken();
+            int len = DNA.length();
+            for (int j = 0; j < n; j++) {
+                cntArr[DNA.charAt(j) - 'A']++;
+            }
+            add();
+            for (int j = n; j < len; j++) {
+                cntArr[DNA.charAt(j) - 'A']++;
+                cntArr[DNA.charAt(j - n) - 'A']--;
+                add();
+            }
+            List<Integer> list = new ArrayList<>(map.values());
+            list.sort((o1, o2) -> o2 - o1);
+            sb.append(list.get(0)).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
