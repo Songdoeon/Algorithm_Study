fece81e23542787836fd439307e6f3d023cbf193
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-04-28 15:16:52 +0900

    [Silver III] Title: MCS, Time: 996 ms, Memory: 204448 KB -BaekjoonHub

--- 백준/Silver/8933. MCS/MCS.java
+++ 백준/Silver/8933. MCS/MCS.java
@@ -1,72 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
+class MCS{
+    int A, G, T, C;
 
-    static int T, K, N, a, g, c, t, ans;
-    static Map<Long, Integer> dna;
-    static String str;
-    static final long H = 601;
+    public MCS(int a, int g, int t, int c) {
+        A = a;
+        G = g;
+        T = t;
+        C = c;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MCS mcs = (MCS) o;
+
+        if (A != mcs.A) return false;
+        if (G != mcs.G) return false;
+        if (T != mcs.T) return false;
+        return C == mcs.C;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = A;
+        result = 31 * result + G;
+        result = 31 * result + T;
+        result = 31 * result + C;
+        return result;
+    }
+}
+class Main{
+    static int N;
+    static int[] cntArr;
+    static Map<MCS, Integer> map;
+    static void add(){
+        MCS mcs = new MCS(cntArr[0], cntArr[6], cntArr[19], cntArr[2]);
+        map.put(mcs, map.getOrDefault(mcs, 0) + 1);
+    }
     public static void main(String[] args) throws IOException {
-        T = Integer.parseInt(br.readLine());
-        while (T-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            K = Integer.parseInt(st.nextToken());
-            str = st.nextToken();
-            sb.append(solution()).append("\n");
-            System.gc();
-        }
-        System.out.print(sb);
-    }
-
-    public static int solution() {
-        ans = 0;
-        N = str.length() - K;
-        dna = new HashMap<>();
-        
-        a = 0;
-        g = 0;
-        c = 0;
-        t = 0;
-
-        for (int i = 0; i < K; i++) add(str.charAt(i));
-        set();
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
         for (int i = 0; i < N; i++) {
-            sub(str.charAt(i));
-            add(str.charAt(i + K));
-            set();
+            cntArr = new int[26];
+            map = new WeakHashMap<>();
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            String DNA = st.nextToken();
+            int len = DNA.length();
+            for (int j = 0; j < n; j++) {
+                cntArr[DNA.charAt(j) - 'A']++;
+            }
+            add();
+            for (int j = n; j < len; j++) {
+                cntArr[DNA.charAt(j) - 'A']++;
+                cntArr[DNA.charAt(j - n) - 'A']--;
+                add();
+            }
+            sb.append(map.values().stream().max(Comparator.comparingInt(o -> o)).get()).append('\n');
         }
-        return ans;
-    }
-
-    static void add(char ch) {
-        if (ch == 'A') a++;
-        else if (ch == 'G') g++;
-        else if (ch == 'C') c++;
-        else if (ch == 'T') t++;
-    }
-
-    static void sub(char ch) {
-        if (ch == 'A') a--;
-        else if (ch == 'G') g--;
-        else if (ch == 'C') c--;
-        else if (ch == 'T') t--;
-    }
-
-    static void set() {
-        long hash = (H * H * H * a) + (H * H * g) + (H * c) + t;
-        if (!dna.containsKey(hash)) dna.put(hash, 0);
-        int v = dna.get(hash) + 1;
-        ans = Math.max(ans, v);
-        dna.put(hash, v);
+        System.out.println(sb);
     }
 }
