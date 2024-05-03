96294496dfdf735b200086955ea789d7704043e8
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-05-03 15:24:25 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 112 ms, Memory: 16348 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -10,15 +10,14 @@ class Node{
     }
 }
 public class Main {
-    static int N, K;
+    
     static int[] visited;
     static Deque<Node> queue = new ArrayDeque<>();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        int N, K;
+        N = in.nextInt();
+        K = in.nextInt();
         visited = new int[100_003];
         Arrays.fill(visited, 100_003);
         visited[K] = 0;
@@ -90,5 +89,32 @@ public class Main {
             }
         }
         System.out.println(time + "\n" + cnt);
+    }
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            while ((c = read()) <= 32);
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0)
+                    buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
     }
 }
