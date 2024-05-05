38097403a2cd0f8a8b31136df1f45471ecdb7d62
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-05-05 12:36:34 +0900

    [Gold IV] Title: 세 수의 합, Time: 292 ms, Memory: 44864 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -5,38 +5,27 @@ import java.io.*;
 class Main {
     static int N;
     static Integer[] arr;
+    static Set<Integer> set = new HashSet<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new Integer[N];
-        int result = -1;
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                set.add(arr[i] + arr[j]);
+            }
         }
         Arrays.sort(arr, Collections.reverseOrder());
 
-        boolean find = false;
-
-        for (int i = 0; i < N - 1; i++) {
+        for (int i = 0; i < N; i++) {
             int n1 = arr[i];
             for (int j = i + 1; j < N; j++) {
-                int n2 = arr[j];
-                int start = j;
-                int end = N - 1;
-                while (start <= end){
-                    int sum = arr[start] + arr[end];
-                    if(n1 == n2 + sum) {
-                        find = true;
-                        result = n1;
-                    }
-                    if(n1 < n2 + sum){
-                        start++;
-                    }
-                    else end--;
+                if(set.contains(n1 - arr[j])){
+                    System.out.println(n1);
+                    return ;
                 }
             }
-            if(find) break;
         }
-        System.out.println(result);
     }
 }
