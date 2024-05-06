d3e7f8c62efe39287c4bd0d189e108c84b58d568
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-05-06 12:11:00 +0900

    [Silver IV] Title: 아~파트 아파트, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/31797. 아～파트 아파트/아～파트 아파트.java
+++ 백준/Silver/31797. 아～파트 아파트/아～파트 아파트.java
@@ -3,17 +3,15 @@ import java.io.*;
 
 class Main{
     static int N, M;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        N = in.nextInt();
+        M = in.nextInt();
         int[] arr = new int[10_001];
         boolean[] isExist = new boolean[10_001];
         for (int i = 1; i <= M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int floor1 = Integer.parseInt(st.nextToken());
-            int floor2 = Integer.parseInt(st.nextToken());
+            int floor1 = in.nextInt();
+            int floor2 = in.nextInt();
             isExist[floor1] = isExist[floor2] = true;
             arr[floor1] = arr[floor2] = i;
 
@@ -26,5 +24,32 @@ class Main{
             if(isExist[idx++]) ans++;
         }
         System.out.println(arr[idx - 1]);
+    }
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            while ((c = read()) <= 32);
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0)
+                    buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
     }
 }
