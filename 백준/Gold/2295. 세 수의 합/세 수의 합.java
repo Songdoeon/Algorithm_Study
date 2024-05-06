832fdbcfdc36bc451d2e776863594e7b100945b2
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-05-06 13:55:53 +0900

    [Gold IV] Title: 세 수의 합, Time: 144 ms, Memory: 60900 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -4,22 +4,22 @@ import java.io.*;
 
 class Main {
     static int N;
-    static Integer[] arr;
+    static int[] arr;
     static BitSet set = new BitSet(400_000_001);
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        arr = new Integer[N];
+        arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
             for (int j = 0; j <= i; j++) {
                 set.set(arr[i] + arr[j]);
             }
         }
-        Arrays.sort(arr, Collections.reverseOrder());
+        Arrays.sort(arr);
 
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
                 if(set.get(arr[i] - arr[j])){
                     System.out.println(arr[i]);
                     return ;
