a7bee20a2d84d9e7c3e337cfe97a669553f4ccc1
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-05-06 19:39:54 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 504 ms, Memory: 63448 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,24 +1,27 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
+public class Main {
 
-class Main {
-    static int N, M;
-    static int[] arr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        Map<String, String> map = new HashMap<>(N * 2);
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            map.put(st.nextToken(), st.nextToken());
-        }
-        for (int i = 0; i < M; i++) {
-            sb.append(map.get(br.readLine())).append('\n');
-        }
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Map<String, String> map = new HashMap<>();
+		for (int n = 0; n < N; n++) {
+			st = new StringTokenizer(br.readLine());
+			map.put(st.nextToken(), st.nextToken());
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int m = 0; m < M; m++) {
+			String s = br.readLine();
+			sb.append(map.get(s)).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
