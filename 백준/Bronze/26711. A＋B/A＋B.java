9d1948573b5cef78a4fbb4a2b63cba97e4ea3ebe
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-05-11 18:20:17 +0900

    [Bronze V] Title: A+B, Time: 132 ms, Memory: 13280 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(new BigInteger(br.readLine()).add(new BigInteger(br.readLine())));
+    }
+}
