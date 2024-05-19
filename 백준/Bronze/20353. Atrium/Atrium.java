da5834e0a03ebffa17d9925bb72d31ec8796ab8f
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-05-19 13:24:25 +0900

    [Bronze IV] Title: Atrium, Time: 80 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Math.sqrt(Double.parseDouble(br.readLine())) * 4);
+    }
+}
