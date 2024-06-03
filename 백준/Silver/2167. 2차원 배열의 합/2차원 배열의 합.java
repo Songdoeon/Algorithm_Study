d1c1cfde906407ac127573dce3df4c1a41775b8a
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-06-03 15:37:22 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 3428 ms, Memory: 52988 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -9,20 +9,21 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder();
-        int[][] arr = new int[N][M];
-        for (int i = 0; i < N; i++) {
+        int[][] arr = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for (int j = 1; j <= M; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int s1 = Integer.parseInt(st.nextToken()) - 1;
-            int s2 = Integer.parseInt(st.nextToken()) - 1;
-            int e1 = Integer.parseInt(st.nextToken()) - 1;
-            int e2 = Integer.parseInt(st.nextToken()) - 1;
+            int s1 = Integer.parseInt(st.nextToken());
+            int s2 = Integer.parseInt(st.nextToken());
+            int e1 = Integer.parseInt(st.nextToken());
+            int e2 = Integer.parseInt(st.nextToken());
             int sum = 0;
             for (int i = s1; i <= e1; i++) {
                 for (int j = s2; j <= e2; j++) {
