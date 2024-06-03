eb43c8f9c22f7634661483ee92c360641d427541
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-06-03 23:06:23 +0900

    [Silver V] Title: 배열 합치기, Time: 1464 ms, Memory: 302512 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -7,18 +7,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Queue<Integer> queue = new PriorityQueue<>();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr1 = new int[N];
+        int[] arr2 = new int[M];
         st = new StringTokenizer(br.readLine());
-        while (st.hasMoreTokens()) {
-            queue.offer(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < N; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
-        while (st.hasMoreTokens()) {
-            queue.offer(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < M; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
-        while (!queue.isEmpty()) {
-            
-            sb.append(queue.poll()).append(' ');
+        int idx1 = 0;
+        int idx2 = 0;
+        while (idx1 < N && idx2 < M) {
+            if (arr1[idx1] < arr2[idx2]) {
+                sb.append(arr1[idx1++]).append(' ');
+            }
+            else {
+                sb.append(arr2[idx2++]).append(' ');
+            }
+        }
+        while (idx1 < N) {
+            sb.append(arr1[idx1++]).append(' ');
+        }
+        while (idx2 < M) {
+            sb.append(arr2[idx2++]).append(' ');
         }
         System.out.println(sb);
     }
