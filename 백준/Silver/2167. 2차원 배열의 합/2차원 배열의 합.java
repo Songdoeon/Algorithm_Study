3d1fa7a141a2c1173f2f6e8db7e9fc9743718598
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-06-03 15:50:49 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 284 ms, Memory: 25752 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -14,7 +14,7 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken()) - arr[i - 1][j - 1] + arr[i][j - 1] + arr[i - 1][j];
             }
         }
         int t = Integer.parseInt(br.readLine());
@@ -24,13 +24,7 @@ public class Main {
             int s2 = Integer.parseInt(st.nextToken());
             int e1 = Integer.parseInt(st.nextToken());
             int e2 = Integer.parseInt(st.nextToken());
-            int sum = 0;
-            for (int i = s1; i <= e1; i++) {
-                for (int j = s2; j <= e2; j++) {
-                    sum += arr[i][j];
-                }
-            }
-            sb.append(sum).append('\n');
+            sb.append(arr[e1][e2] - arr[s1 - 1][e2] - arr[e1][s2 - 1] + arr[s1 - 1][s2 - 1]).append('\n');
         }
         System.out.println(sb);
     }
