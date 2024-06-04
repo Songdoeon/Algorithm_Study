2dab158cb0a8481bc302ab6717e0e496e595fde3
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-06-04 21:26:44 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -1,8 +1,6 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
