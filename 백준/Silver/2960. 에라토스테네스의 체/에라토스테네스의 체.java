f44de44f82d75b7397bb34195eb934f02eceb137
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-06-04 12:36:35 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 72 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -5,22 +5,19 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] arr = new int[nums[0] + 1];
-
-        for (int i = 2; i <= nums[0]; i++) {
-            arr[i] = i;
-        }
-        for (int i = 2; i <= nums[0]; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        Arrays.fill(arr, 2);
+        for (int i = 2; i <= n; i++) {
             if (arr[i] == 0) continue;
-            for (int j = i; j <= nums[0]; j+=i) {
-                if (arr[j] != 0) {
-                    arr[j] = 0;
-                    nums[1]--;
-                    if (nums[1] == 0) {
-                        System.out.print(j);
-                        return;
-                    }
+            for (int j = i; j <= n; j += i) {
+                if(arr[j] == 0) continue;
+                arr[j] = 0;
+                if (--m == 0) {
+                    System.out.print(j);
+                    return;
                 }
             }
         }
