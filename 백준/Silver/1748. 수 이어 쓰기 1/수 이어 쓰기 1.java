f218fc72deedffea9e7a8865931000f881f59e62
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-06-09 13:32:37 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 76 ms, Memory: 11388 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -2,21 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static int N;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int count = 0;
-        int plus = 1;
-        int num = 10;
-        for(int i = 1; i <= N; i++) {
-            if(i%num == 0) {
-                plus++;
-                num *= 10;
-            }
-            count += plus;
-        }
-        System.out.println(count);
+		N = Integer.parseInt(br.readLine());
+		int result = 0;
+        for (int i = 1; i <= N; i *= 10) {
+			result += N - i + 1; 
+		}
+		System.out.println(result);
     }
 }
