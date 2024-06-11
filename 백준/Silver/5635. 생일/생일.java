975ffd7f19c7ebb55452de2506f87e691717f749
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-06-11 16:40:15 +0900

    [Silver V] Title: 생일, Time: 72 ms, Memory: 11332 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.java
+++ 백준/Silver/5635. 생일/생일.java
@@ -1,30 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-class Person{
-    String name;
-    int days;
-
-    public Person(String line) {
-        String[] info = line.split(" ");
-        name = info[0];
-        days = Integer.parseInt(info[1]);
-        days += Integer.parseInt(info[2]) * 30;
-        days += Integer.parseInt(info[3]) * 365;
-    }
-}
 public class Main {
     static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Person> list = new ArrayList<>();
         N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            list.add(new Person(br.readLine()));
-        }
-        list.sort(Comparator.comparingInt(o -> o.days));
+        String minName = "", maxName = "";
+        int minAge = 999_999_999, maxAge = 0;
 
-        System.out.println(list.get(N - 1).name + "\n" + list.get(0).name);
+        for (int i = 0; i < N; i++) {
+            String[] info = br.readLine().split(" ");
+            String name = info[0];
+            int days = Integer.parseInt(info[1]);
+            days += Integer.parseInt(info[2]) * 30;
+            days += Integer.parseInt(info[3]) * 365;
+            if(days > maxAge) {
+                maxAge = days;
+                maxName = name;
+            }
+            else if(days < minAge) {
+                minAge = days;
+                minName = name;
+            }
+        }
+        System.out.println(maxName + "\n" + minName);
     }
 }
