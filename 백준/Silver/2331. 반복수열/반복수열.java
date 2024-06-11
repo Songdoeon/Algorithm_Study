36e1b884ccc9279fa7b62ea94c35aa28b35a7d6e
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-06-11 17:58:50 +0900

    [Silver IV] Title: 반복수열, Time: 76 ms, Memory: 13368 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.java
+++ 백준/Silver/2331. 반복수열/반복수열.java
@@ -6,13 +6,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Set<Integer> set = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[500_000];
         int num = Integer.parseInt(st.nextToken());
         int pow = Integer.parseInt(st.nextToken());
-
-        while (!set.contains(num)) {
-            set.add(num);
+        int cnt = 1;
+        while (arr[num] == 0) {
+            arr[num] = cnt++;
             String s = num + "";
             num = 0;
             int len = s.length();
@@ -20,15 +20,6 @@ public class Main {
                 num += (int) Math.pow(s.charAt(i) - '0', pow);
             }
         }
-        while (set.contains(num)){
-            set.remove(num);
-            String s = num + "";
-            num = 0;
-            int len = s.length();
-            for (int i = 0; i < len; i++) {
-                num += (int) Math.pow(s.charAt(i) - '0', pow);
-            }
-        }
-        System.out.println(set.size());
+        System.out.println(arr[num] - 1);
     }
 }
