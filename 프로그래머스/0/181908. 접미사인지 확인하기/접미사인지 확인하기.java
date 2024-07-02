c45f0c9356081b0bf80080362929743f6a4d534b
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-07-02 15:19:27 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        int len = my_string.length();
+        
+        for(int i = 0; i < len; i++){
+            if(is_suffix.equals(my_string.substring(i))) {
+                answer = 1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
