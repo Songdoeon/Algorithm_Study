2a8bbf006e2ecd26a3c0dd34245d8a3f6989bd3e
songdoeon <96420547+Songdoeon@users.noreply.github.com>
2024-07-18 22:25:39 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11236 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,13 +1,1 @@
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.print("         ,r'\"7\n" +
-                "r`-_   ,'  ,/\n" +
-                " \\. \". L_r'\n" +
-                "   `~\\/\n" +
-                "      |\n" +
-                "      |");
-    }
-}
+interface Main{static void main(String[] a){System.out.print("         ,r'\"7\n" +"r`-_   ,'  ,/\n"+" \\. \". L_r'\n"+"   `~\\/\n"+"      |\n"+"      |");}}
