c905cc9a97752541cc5ca452a83732e8b5249a7e
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-07-31 09:01:11 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 260 ms, Memory: 24528 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int ans = 0;
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            String exec = br.readLine();
+            if(exec.equals("ENTER")) {
+                ans += set.size();
+                set.clear();
+            }
+            else set.add(exec);
+        }
+        System.out.println(ans + set.size());
+    }
+}
