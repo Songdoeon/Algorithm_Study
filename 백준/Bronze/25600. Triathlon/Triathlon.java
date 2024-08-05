d4b6e8cec70a69ab9f92c858bdb510c7853cddf7
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-08-06 08:53:13 +0900

    [Bronze IV] Title: Triathlon, Time: 80 ms, Memory: 12116 KB -BaekjoonHub

---
+++ 백준/Bronze/25600. Triathlon/Triathlon.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            int sum = a == (b + c) ? 2 * a * (b + c) : a * (b + c);
+            ans = Math.max(ans, sum);
+        }
+        System.out.println(ans);
+    }
+}
