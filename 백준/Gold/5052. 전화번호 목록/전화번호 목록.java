09780c5deebd2cc5de3bb4190041ae49b22fae12
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-08-16 15:44:09 +0900

    [Gold IV] Title: 전화번호 목록, Time: 856 ms, Memory: 137496 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int T, N;
+    static String[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        T = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            boolean find = false;
+            N = Integer.parseInt(br.readLine());
+            arr = new String[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = br.readLine().replace(" ","");
+            }
+            Arrays.sort(arr, String::compareTo);
+            for (int i = 0; i < N - 1; i++) {
+                if (arr[i + 1].startsWith(arr[i])) {
+                    find = true;
+                    sb.append("NO").append('\n');
+                    break;
+                }
+            }
+            if (!find) {
+                sb.append("YES").append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
