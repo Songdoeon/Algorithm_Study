74bc4221dda8f899f119844c701a56ec796bff20
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-08-27 09:47:53 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 100 ms, Memory: 12708 KB -BaekjoonHub

---
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr, dp;
+
+    static int check(int mid){
+        int cnt = 1;
+        int s = 10_001;
+        int e = 0;
+        for (int i = 1; i <= N; i++) {
+            s = Math.min(s, arr[i]);
+            e = Math.max(e, arr[i]);
+
+            if(e - s > mid) {
+//                System.out.println(e + " - " + s);
+                i--;
+                cnt++;
+                s = 10_001;
+                e = 0;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N + 1];
+        dp = new int[N + 1];
+
+        int min = 10_001;
+        int max = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = dp[i - 1] + arr[i];
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        int s = 0;
+        int e = (max - min) + 1;
+        int mid;
+        int ans = 10_001;
+        while(s < e){
+            mid = (s + e) / 2;
+            int cnt = check(mid);
+            if(cnt <= M){
+                e = mid;
+                ans = mid;
+            }
+            else {
+                s = mid + 1;
+            }
+        }
+        System.out.println(ans == 10_001 ? 0 : ans);
+    }
+}
