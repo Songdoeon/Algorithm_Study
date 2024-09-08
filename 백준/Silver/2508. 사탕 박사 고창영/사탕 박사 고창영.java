8dfffaf2396090796d6f7afa33f787be2d3614ca
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-09-08 22:52:53 +0900

    [Silver V] Title: 사탕 박사 고창영, Time: 144 ms, Memory: 20208 KB -BaekjoonHub

--- 백준/Silver/2508. 사탕 박사 고창영/사탕 박사 고창영.java
+++ 백준/Silver/2508. 사탕 박사 고창영/사탕 박사 고창영.java
@@ -21,15 +21,29 @@ class Main {
             map = new char[N][M];
             for (int i = 0; i < N; i++) {
                 map[i] = br.readLine().toCharArray();
-                for (int j = 0; j < M; j++) {
+            }
+            for (int i = 2; i < N; i++) {
+                for (int j = 0; j < 2; j++) {
+                    if(map[i][j] == column[0] &&
+                        map[i - 1][j] == column[1] &&
+                        map[i - 2][j] == column[2]) cnt++;
+                }
+            }
+            for (int i = 2; i < N; i++) {
+                for (int j = 2; j < M; j++) {
                     if(map[i][j] == row[0] &&
-                            j >= 2  &&
                             map[i][j - 1] == row[1] &&
                             map[i][j - 2] == row[2]) cnt++;
                     else if(map[i][j] == column[0] &&
-                            i >= 2  &&
                             map[i - 1][j] == column[1] &&
                             map[i - 2][j] == column[2]) cnt++;
+                }
+            }
+            for (int i = 0; i < 2; i++) {
+                for (int j = 2; j < M; j++) {
+                    if(map[i][j] == row[0] &&
+                            map[i][j - 1] == row[1] &&
+                            map[i][j - 2] == row[2]) cnt++;
                 }
             }
             sb.append(cnt).append('\n');
