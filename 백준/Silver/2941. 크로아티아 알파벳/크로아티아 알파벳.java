b962fd69cbd29eace17c6f5f496bf889a50cc621
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-09-11 17:53:45 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 64 ms, Memory: 11612 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,25 +1,49 @@
-import java.util.*;
 import java.io.*;
 
-public class Main
-{
-	static int answer = 0;
+public class Main {
+    static int answer = 0;
     static String input;
-    static String alpha[] = {"dz=", "c=", "c-", "d-", "lj", "nj", "s=", "z="};
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		input = br.readLine();
-		
-		int len = input.length();
-		int cnt = 0;
-		for(int i = 0; i < 8; i++){
-		    input = input.replace(alpha[i], ".");
-		    
-		    answer += (len - input.length()) / (alpha[i].length()-1);
-		    len = input.length();
-		}
-		input = input.replace(".", "");
-		System.out.println(answer + input.length());
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        input = br.readLine();
+
+        int len = input.length();
+
+        input = input.replace("dz=", ".");
+
+        answer += (len - input.length()) / 2;
+        len = input.length();
+
+        input = input.replace("c=", ".");
+
+        answer += (len - input.length());
+        len = input.length();
+        input = input.replace("c-", ".");
+
+        answer += (len - input.length());
+        len = input.length();
+        input = input.replace("d-", ".");
+
+        answer += (len - input.length());
+        len = input.length();
+        input = input.replace("lj", ".");
+
+        answer += (len - input.length());
+        len = input.length();
+        input = input.replace("nj", ".");
+
+        answer += (len - input.length());
+        len = input.length();
+        input = input.replace("s=", ".");
+
+        answer += (len - input.length());
+        len = input.length();
+        input = input.replace("z=", ".");
+
+        answer += (len - input.length());
+        
+        input = input.replace(".", "");
+        System.out.println(answer + input.length());
+    }
 }
