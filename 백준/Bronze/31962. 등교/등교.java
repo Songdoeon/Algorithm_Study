5b7215844c4a201f7c2a31ae4c024fa6eda1705e
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-09-28 19:01:28 +0900

    [Bronze IV] Title: 등교, Time: 68 ms, Memory: 11624 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31962. 등교/등교.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int ans = -1;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            if(s + e > M) continue;
+            ans = Math.max(ans, s);
+        }
+        System.out.println(ans);
+    }
+}
