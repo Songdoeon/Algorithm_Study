0007113b739e06c0eb811d348c9f1a54db143987
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-09-30 19:11:32 +0900

    [Bronze IV] Title: 양말 짝 맞추기, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Bronze/28431. 양말 짝 맞추기/양말 짝 맞추기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int T, N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10];
+
+        for (int i = 0; i < 5; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+        for (int i = 0; i < 10; i++) {
+            if(arr[i] % 2 == 1) {
+                System.out.println(i);
+                return ;
+            }
+        }
+    }
+}
