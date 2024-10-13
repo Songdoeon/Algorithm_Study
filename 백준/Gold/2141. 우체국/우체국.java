9eee1d6503c0f37ab4e7a36ae0bc7c9b52351732
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-10-13 23:50:05 +0900

    [Gold IV] Title: 우체국, Time: 540 ms, Memory: 51608 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.java
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -14,25 +14,18 @@ public class Main {
     static Office[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         long total = 0;
         arr = new Office[N];
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             x = Integer.parseInt(st.nextToken());
             y = Integer.parseInt(st.nextToken());
             total += y;
-            min = Math.min(min, x);
-            max = Math.max(max, x);
             arr[i] = new Office(x, y);
         }
         Arrays.sort(arr, Comparator.comparingInt(o -> o.x));
-        long sum = 0;
-
         int s = 0;
         int e = N - 1;
         long cnt = 0;
@@ -40,10 +33,8 @@ public class Main {
 
         while (s < e){
             cnt += arr[s].y;
-            if(cnt == flag) break;
-            else if(cnt < flag) s++;
+            if(cnt < flag) s++;
             else break;
-
         }
         System.out.println(arr[s].x);
     }
