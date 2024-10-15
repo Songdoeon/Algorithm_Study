e5a6575ab8e8af0fcb151e57d150c1b9700aa268
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-10-15 10:52:05 +0900

    [Gold III] Title: 역사, Time: 464 ms, Memory: 37196 KB -BaekjoonHub

---
+++ 백준/Gold/1613. 역사/역사.java
@@ -0,0 +1,61 @@
+import org.w3c.dom.Node;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    static int N, M;
+    static int[][] arr;
+    static final int INF = 100_000_000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1][N + 1];
+        for (int i = 0; i <= N; i++) {
+            Arrays.fill(arr[i], INF);
+        }
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            arr[s][e] = 1;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if(i == j) {
+                        arr[i][j] = 0;
+                    }
+                    if(arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+        boolean first = false;
+        boolean second = false;
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            if(arr[s][e] != INF) first = true;
+            if(arr[e][s] != INF) second = true;
+            if(!first && !second) sb.append(0).append('\n');
+            else if(first) sb.append(-1).append('\n');
+            else sb.append(1).append('\n');
+            first = second = false;
+        }
+
+        System.out.println(sb);
+    }
+}
