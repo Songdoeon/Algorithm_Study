4f8e6a00d14c0a611821eb97acc3575e044f4652
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-11-01 22:53:59 +0900

    [Gold IV] Title: 케이크 자르기, Time: 120 ms, Memory: 11956 KB -BaekjoonHub

---
+++ 백준/Gold/17179. 케이크 자르기/케이크 자르기.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    static int N, M, L, C;
+    static int[] src;
+    static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		src = new int[M+2];
+
+		for(int i=1; i<=M; i++) {
+		    src[i] = Integer.parseInt(br.readLine());
+		}
+		src[0] = 0;
+		src[M+1] = L;
+	    
+	    
+	    for(int i=0; i<N; i++) {
+	        C = Integer.parseInt(br.readLine());
+	        int lt =  1;
+    	    int rt = L;
+    	    int answer = 0;
+    	    while(lt<=rt){
+    	        int mid = (lt+rt)/2;
+    	        if(canDivide(mid)){
+    	            answer = Math.max(answer, mid);
+    	            lt = mid+1;
+    	        }else{
+    	            rt = mid-1;
+    	        }
+    	    }    
+    	    sb.append(answer).append("\n");
+	    }
+	    System.out.println(sb);
+		br.close();
+	}
+	public static boolean canDivide(int target) {
+	    int count = 0;
+	    int last = 0;
+	    for(int i=1; i<=M+1; i++) {
+	        if(src[i] - src[last] >= target) {
+	            count++;
+	            last = i;
+	        }
+	    }
+	   // System.out.println(target + " - " + count);
+	    return count >= C+1;
+	}
+}
