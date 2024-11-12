d4bc4479154a23e9c05068492d8da1a84bb3263e
송도언 <96420547+Songdoeon@users.noreply.github.com>
2024-11-13 08:38:12 +0900

    [Bronze IV] Title: 호반우가 학교에 지각한 이유 1, Time: 68 ms, Memory: 11576 KB -BaekjoonHub

---
+++ 백준/Bronze/30468. 호반우가 학교에 지각한 이유 1/호반우가 학교에 지각한 이유 1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main extends Exception {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int STR = Integer.parseInt(st.nextToken());
+        int DEX = Integer.parseInt(st.nextToken());
+        int INT = Integer.parseInt(st.nextToken());
+        int LUK = Integer.parseInt(st.nextToken());
+        int avg = Integer.parseInt(st.nextToken());
+
+        int current = (STR + DEX + INT + LUK);
+
+        int answer = (avg * 4 - current);
+
+        if (current >= avg * 4){
+            answer = 0;
+        }
+
+        sb.append(answer);
+        System.out.println(sb.toString());
+    }
+}
