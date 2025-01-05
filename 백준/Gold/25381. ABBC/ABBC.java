9114272fd18cdf13a95728912d4de5ebcd2420a2
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-01-05 20:34:32 +0900

    [Gold III] Title: ABBC, Time: 136 ms, Memory: 17156 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.java
+++ 백준/Gold/25381. ABBC/ABBC.java
@@ -1,19 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
     static String word;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         word = br.readLine();
-        int len = word.length() - 1;
+        int len = word.length();
         int a = 0;
         int b = 0;
         int c = 0;
         int ans = 0;
-        for (int i = len; i >= 0; i--) {
+        for (int i = len - 1; i >= 0; i--) {
             char ch = word.charAt(i);
             if(ch == 'C') c++;
             else if(ch == 'B'){
@@ -33,6 +31,28 @@ public class Main {
                 }
             }
         }
-       System.out.println(ans);
+        a = b = c = 0;
+        int ans2 = 0;
+        for (int i = 0; i < len; i++) {
+            char ch = word.charAt(i);
+            if(ch == 'A') a++;
+            else if(ch == 'B'){
+                b++;
+                if(a > 0){
+                    a--;
+                    b--;
+                    ans2++;
+                }
+            }
+            else{
+                c++;
+                if(b > 0){
+                    b--;
+                    c--;
+                    ans2++;
+                }
+            }
+        }
+        System.out.println(Math.max(ans, ans2));
     }
 }
