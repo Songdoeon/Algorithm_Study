ff530f4f9f3a7e4ab1c296d773ebbf01b979504c
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-01-07 23:23:35 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 204 ms, Memory: 22324 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -1,16 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-class Building{
-    int high, cnt;
-
-    public Building(int high, int cnt) {
-        this.high = high;
-        this.cnt = cnt;
-    }
-}
 public class Main {
     static int N;
     public static void main(String[] args) throws IOException {
@@ -18,42 +11,17 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
 
-        Stack<Building> stack = new Stack<>();
-
         long ans = 0;
+
+        Deque<Integer> stack =  new ArrayDeque<>();
 
         for (int i = 0; i < N; i++) {
             int n = Integer.parseInt(br.readLine());
-            Building b = new Building(n, 0);
-//            System.out.println(n + " 시작");
-            while(!stack.isEmpty()){
-                Building prev = stack.peek();
-//                System.out.println("prev : " + prev.high + " cnt : " + prev.cnt);
-                if(prev.high < n) {
-                    int h = stack.pop().cnt;
-                    ans += h;
-                    if(!stack.isEmpty()) stack.peek().cnt += h;
-//                    stack.push(b);
-                }
-                else if(prev.high > n){
-                    prev.cnt++;
-//                    stack.push(b);
-                    break;
-                }
-                else{
-//                    System.out.println("여긴 오지마라");
-                    if(stack.size() > 1) stack.get(stack.size() - 2).cnt++;
-                    break;
-                }
+            while (!stack.isEmpty() && n >= stack.peek()) {
+                stack.pop();
             }
-            if(stack.isEmpty() || stack.peek().high != n) stack.push(b);
-        }
-//        System.out.println("size : " + stack.size());
-        int ret = 0;
-        while (!stack.isEmpty()) {
-            ret += stack.pop().cnt;
-            ans += ret;
-//            System.out.println(stack.size() + " : " + ans);
+            ans += stack.size();
+            stack.push(n);
         }
         System.out.println(ans);
     }
