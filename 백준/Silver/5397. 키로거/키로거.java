b280552c7e0f2f77e5aa71deba6e2e025b0e7172
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-01-19 13:57:48 +0900

    [Silver II] Title: 키로거, Time: 1120 ms, Memory: 291184 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,48 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Stack;
 
 public class Main {
-    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        N = Integer.parseInt(br.readLine());
+        int testCases = Integer.parseInt(br.readLine());
 
+        for (int t = 0; t < testCases; t++) {
+            String input = br.readLine();
+            Stack<Character> left = new Stack<>();
+            Stack<Character> right = new Stack<>();
 
-        for (int i = 0; i < N; i++) {
-            int idx = 0;
-            char[] arr = br.readLine().toCharArray();
-//            StringBuilder tempStr = new StringBuilder();
-            List<Character> list = new LinkedList<>();
-            int len = arr.length;
-
-            for (int j = 0; j < len; j++) {
-                char exec = arr[j];
-                if(exec == '<'){
-                    if(idx > 0) idx--;
-                }
-                else if(exec == '>') {
-                    if(idx != list.size()) idx++;
-                }
-                else if(exec == '-') {
-                    if(idx != 0) {
-                        list.remove(--idx);
+            for (char ch : input.toCharArray()) {
+                if (ch == '-') { // 백스페이스
+                    if (!left.isEmpty()) {
+                        left.pop();
                     }
-                }
-                else {
-                    if(idx == list.size()) list.add(exec);
-                    else list.add(idx, exec);
-                    idx++;
+                } else if (ch == '<') { // 왼쪽 화살표
+                    if (!left.isEmpty()) {
+                        right.push(left.pop());
+                    }
+                } else if (ch == '>') { // 오른쪽 화살표
+                    if (!right.isEmpty()) {
+                        left.push(right.pop());
+                    }
+                } else { // 일반 문자
+                    left.push(ch);
                 }
             }
-            for(Character c : list){
-                sb.append(c);
+
+            // 결과를 StringBuilder로 구성
+            StringBuilder result = new StringBuilder();
+            // left 스택을 역순으로 추가
+            while (!left.isEmpty()) {
+                result.append(left.pop());
             }
-            sb.append('\n');
+            result.reverse(); // 역순으로 뒤집기
+
+            // right 스택을 뒤에 추가
+            StringBuilder rightResult = new StringBuilder();
+            while (!right.isEmpty()) {
+                rightResult.append(right.pop());
+            }
+            result.append(rightResult);
+
+            System.out.println(result);
         }
-        System.out.println(sb);
     }
 }
