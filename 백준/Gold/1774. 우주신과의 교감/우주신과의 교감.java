0e742ff64210f2cce737653ead01f9b9126792c9
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-02-15 13:16:17 +0900

    [Gold III] Title: 우주신과의 교감, Time: 560 ms, Memory: 41480 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
@@ -6,10 +6,11 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Node {
+
+class Node{
     double x, y;
 
-    public Node(double x, double y) {
+    public Node(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -23,40 +24,35 @@ class Node {
     }
 }
 
-class Edge {
-    int start, end;
-    double cost;
-
-    public Edge(int start, int end, double cost) {
-        this.start = start;
-        this.end = end;
-        this.cost = cost;
+class Edge{
+    int x, y;
+    double c;
+    public Edge(int x, int y, double c) {
+        this.x = x;
+        this.y = y;
+        this.c = c;
     }
 }
-
 class Main {
     static int N, M;
     static int[] parents;
     static Node[] nodes;
-
-    static int find(int n) {
-        if (n == parents[n]) return n;
+    static int find(int n){
+        if(n == parents[n]) return n;
         return parents[n] = find(parents[n]);
     }
-
-    static boolean union(int n, int m) {
+    static boolean union(int n, int m){
         int p1 = find(n);
         int p2 = find(m);
-        if (p1 == p2) return false;
-        if (p1 > p2) parents[p1] = p2;
+        if(p1 == p2) return false;
+        if(p1 > p2) parents[p1] = p2;
         else parents[p2] = p1;
         return true;
     }
 
-    static double getDistance(int x, int y) {
-        return Math.sqrt(Math.pow(nodes[x].x - nodes[y].x, 2) + Math.pow(nodes[x].y - nodes[y].y, 2));
+    static double getDis(int x, int y){
+        return Math.sqrt(Math.pow(nodes[x].x - nodes[y].x, 2) +  Math.pow(nodes[x].y - nodes[y].y, 2));
     }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -64,7 +60,7 @@ class Main {
         M = Integer.parseInt(st.nextToken());
 
         parents = new int[N + 1];
-        Queue<Edge> edgeQueue = new PriorityQueue<>(Comparator.comparingDouble(o -> o.cost));
+        Queue<Edge> queue = new PriorityQueue<>(Comparator.comparingDouble(o -> o.c));
         for (int i = 1; i <= N; i++) {
             parents[i] = i;
         }
@@ -72,35 +68,30 @@ class Main {
         nodes = new Node[N + 1];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            nodes[i] = new Node(Double.parseDouble(st.nextToken()), Double.parseDouble(st.nextToken()));
+            nodes[i] = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
         for (int i = 1; i <= N; i++) {
             for (int j = i + 1; j <= N; j++) {
-                edgeQueue.add(new Edge(i, j, getDistance(i, j)));
+                queue.add(new Edge(i, j, getDis(i, j)));
             }
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            union(u, v);
+            union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
-
-        double totalCost = 0;
-        int edgesUsed = 0;
-
-        while (!edgeQueue.isEmpty() && edgesUsed < N - 1) {
-            Edge edge = edgeQueue.poll();
-            int x = edge.start;
-            int y = edge.end;
-            if (union(x, y)) {
-                totalCost += edge.cost;
-                edgesUsed++;
+        double ans = 0;
+        int edge = 0;
+        while (!queue.isEmpty() && edge < N - 1) {
+            Edge e = queue.poll();
+            int x = e.x;
+            int y = e.y;
+            if(union(x, y)){
+                ans += e.c;
+                edge++;
             }
         }
-
-        System.out.printf("%.2f\n", totalCost);
+        System.out.printf("%.2f", ans);
     }
 }
