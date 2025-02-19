6e5002aa50cb1c37dd69d24eab8dbff7f35898e6
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-02-19 12:20:20 +0900

    [Gold IV] Title: 줄세우기, Time: 68 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+class Main {
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int[] dp = new int[N];
+        int idx = 0;
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= idx; j++) {
+                if(arr[i] < dp[j]) {
+                    if(j == idx)idx++;
+                    dp[j] = arr[i];
+                    break;
+                }
+            }
+        }
+        System.out.println(N - idx);
+    }
+}
