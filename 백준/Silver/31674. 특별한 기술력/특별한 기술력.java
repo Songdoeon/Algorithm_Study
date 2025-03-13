03e18f8719ae114f611822b3fc870714a3187221
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-03-13 11:15:42 +0900

    [Silver II] Title: 특별한 기술력, Time: 428 ms, Memory: 49212 KB -BaekjoonHub

---
+++ 백준/Silver/31674. 특별한 기술력/특별한 기술력.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, K;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        long ans = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            ans += (arr[i] + ans) % 1_000_000_007;
+        }
+        System.out.println(ans % 1_000_000_007);
+    }
+}
