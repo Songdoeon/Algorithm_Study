e3e5cde58ee37b255cb5c9b01c69b699a22a717f
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-03-15 10:37:20 +0900

    [Gold V] Title: 0 만들기, Time: 60 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -4,24 +4,24 @@ import java.io.InputStreamReader;
 
 public class Main {
     static int T, N;
-    static char[] oper;
+    static char[] oper = new char[9];
     static StringBuilder sb = new StringBuilder();
     static void search(int depth, int sum, int num, int op){
         if(depth == N){
             if(sum + (num * op)== 0){
                 for (int i = 1; i < N; i++) {
-                    sb.append(i).append(oper[i - 1]);
+                    sb.append(i).append(oper[i]);
                 }
                 sb.append(N);
                 sb.append('\n');
             }
             return ;
         }
-        oper[depth - 1] = ' ';
+        oper[depth] = ' ';
         search(depth + 1, sum, (num * 10) + depth + 1, op);
-        oper[depth - 1] = '+';
+        oper[depth] = '+';
         search(depth + 1, sum + (num * op), depth + 1, 1);
-        oper[depth - 1] = '-';
+        oper[depth] = '-';
         search(depth + 1, sum + (num * op), depth + 1, -1);
     }
     public static void main(String[] args) throws IOException {
@@ -29,7 +29,6 @@ public class Main {
         T = Integer.parseInt(br.readLine());
         while (T-- > 0){
             N = Integer.parseInt(br.readLine());
-            oper = new char[N];
             search(1, 0, 1, 1);
             sb.append('\n');
         }
