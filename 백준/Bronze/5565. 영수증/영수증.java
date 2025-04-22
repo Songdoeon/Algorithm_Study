f9dc6d2519b936862016ef1205d8986c1753496b
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-04-22 20:08:39 +0900

    [Bronze IV] Title: 영수증, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= 9; i++) {
+            total -= Integer.parseInt(br.readLine());
+        }
+        System.out.print(total);
+    }
+} 
