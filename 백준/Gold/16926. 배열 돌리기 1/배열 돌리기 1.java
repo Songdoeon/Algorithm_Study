d962f062864656fe18b694181601235c8e22ef8d
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-05-13 09:52:25 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 576 ms, Memory: 32568 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -6,21 +6,24 @@ import java.util.StringTokenizer;
 public class Main {
     static int N, M, R;
     static int[][] map;
-    static StringBuilder sb = new StringBuilder();
-    static boolean outOfMap(int x, int y, int d){
-        return x < d || x >= N - d || y < d || y >= M - d;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static boolean outOfMap(int x, int y, int i){
+        return x < i || x >= N - i || y < i || y >= M - i;
     }
     static void rotate(){
-        while (R-- > 0){
+        while(R-- > 0){
             int layer = Math.min(N, M) / 2;
-            for (int l = 0; l < layer; l++) {
-                int prev = map[l][l];
-                int x = l, y = l;
-                for(int[] d : new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}){
-                    while(true){
-                        int nx = x + d[0];
-                        int ny = y + d[1];
-                        if(outOfMap(nx, ny, l))break;
+            for (int i = 0; i < layer; i++) {
+                int prev = map[i][i];
+                int x = i, y = i;
+
+                for (int j = 0; j < 4; j++) {
+                    while (true){
+                        int nx = x + dx[j];
+                        int ny = y + dy[j];
+                        if(outOfMap(nx, ny, i))break;
                         int temp = map[nx][ny];
                         map[nx][ny] = prev;
                         prev = temp;
@@ -28,9 +31,7 @@ public class Main {
                         y = ny;
                     }
                 }
-
             }
-
         }
     }
     public static void main(String[] args) throws IOException {
@@ -47,7 +48,11 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
         rotate();
+
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 sb.append(map[i][j]).append(' ');
