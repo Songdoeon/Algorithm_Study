6ef75799171cf370e7f398cb1ce271865233e724
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-05-31 17:57:29 +0900

    [Bronze IV] Title: Left Beehind, Time: 72 ms, Memory: 11344 KB -BaekjoonHub

---
+++ 백준/Bronze/24768. Left Beehind/Left Beehind.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x == 0 && y == 0) {
+                break;
+            }
+
+            if (x + y == 13) {
+                System.out.println("Never speak again.");
+            } else if (x > y) {
+                System.out.println("To the convention.");
+            } else if (x < y) {
+                System.out.println("Left beehind.");
+            } else {
+                System.out.println("Undecided.");
+            }
+        }
+    }
+}
