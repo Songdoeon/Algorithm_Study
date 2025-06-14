969d3e048b3737dc4055d793d55de94b68355a89
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-06-14 09:20:54 +0900

    [Bronze V] Title: Metronome, Time: 68 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int N, M, K;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print((double)Integer.parseInt(br.readLine()) / 4);
+    }
+}
