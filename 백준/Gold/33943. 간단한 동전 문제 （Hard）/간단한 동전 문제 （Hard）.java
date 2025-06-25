7a12f8fc645734cdd0505d6e489986523b1243ca
송도언 <96420547+Songdoeon@users.noreply.github.com>
2025-06-25 09:57:20 +0900

    [Gold IV] Title: 간단한 동전 문제 (Hard), Time: 268 ms, Memory: 13552 KB -BaekjoonHub

---
+++ 백준/Gold/33943. 간단한 동전 문제 （Hard）/간단한 동전 문제 （Hard）.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean hasZero = false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if(N == 0) {
+            if(M == 0) System.out.println(0);
+            else System.out.println(-1);
+            return ;
+        }
+        int[] P = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        final int OFFSET = 10000;
+        final int MAXV   = 20000;
+
+        int[] dist = new int[MAXV + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Deque<Integer> q = new ArrayDeque<>();
+
+        dist[OFFSET] = 0;
+        q.add(OFFSET);
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            int d = dist[u];
+            if (u == M + OFFSET) break;
+
+            for (int coin : P) {
+                int v = u + coin;
+                if (v < 0 || v > MAXV) continue;
+
+                if (dist[v] > d + 1) {
+                    dist[v] = d + 1;
+                    q.add(v);
+                }
+            }
+        }
+
+        int ans = dist[M + OFFSET];
+        System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+    }
+}
