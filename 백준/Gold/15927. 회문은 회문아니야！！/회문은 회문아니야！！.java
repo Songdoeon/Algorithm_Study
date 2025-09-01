31eace50222c478391d4c73bded81ddc2f0f0eda
DoeonSong <96420547+Songdoeon@users.noreply.github.com>
2025-09-01 09:00:16 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 144 ms, Memory: 17120 KB -BaekjoonHub

---
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+// 15927
+
+public class Main {
+    static int N;
+    static String s;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        s = br.readLine();
+        N = s.length();
+
+        int i = 0, j = N - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                System.out.println(N);
+                return;
+            }
+            i++;
+            j--;
+        }
+        
+        boolean flag = true;
+        for (int k = 1; k < N; k++) {
+            if (s.charAt(k) != s.charAt(0)) { 
+                flag = false; 
+                break; 
+            }
+        }
+        
+        System.out.println(flag ? -1 : N - 1);
+    }
+}
