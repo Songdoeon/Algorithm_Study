866ab37d5d4583cd50b0fbdf6313a7ee70d411c9
DoeonSong <96420547+Songdoeon@users.noreply.github.com>
2025-09-02 19:02:39 +0900

    [Silver II] Title: 과일 서리, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/17213. 과일 서리/과일 서리.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		long up = 1;
+		long down = 1;
+		for(int i = M - N + 1;i <= M - 1; i++) up *= i;
+		
+		for(int i = 1; i <= N - 1; i++) down *= i;
+		
+		System.out.println(up/down);
+		
+	}
+}
