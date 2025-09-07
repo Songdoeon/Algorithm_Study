69dd180e5a2096c7382fe7eaf456c6b6ad89c2d6
DoeonSong <96420547+Songdoeon@users.noreply.github.com>
2025-09-07 18:55:03 +0900

    [Silver IV] Title: 온라인 판매, Time: 72 ms, Memory: 11996 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Integer P[] = new Integer[M];
+        for(int i =0;i<M;i++){
+            P[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(P, Collections.reverseOrder());
+
+
+        int price = 0;
+        int maxPrice = 0;
+        int range = Math.min(M,N);
+        for(int i=0;i<range;i++){
+
+            int tempPrice = (i+1) * P[i];
+
+            if(maxPrice < tempPrice){
+                maxPrice = tempPrice;
+                price = P[i];
+            }
+        }
+        System.out.println(price+" "+maxPrice);
+    }
+}
