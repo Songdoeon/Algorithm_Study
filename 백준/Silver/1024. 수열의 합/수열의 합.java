1966ea95b381618ac57a6095340ed983d55a789b
DoeonSong <96420547+Songdoeon@users.noreply.github.com>
2025-09-28 13:03:25 +0900

    [Silver II] Title: 수열의 합, Time: 72 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Silver/1024. 수열의 합/수열의 합.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, L;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		
+		while(true) {
+			int min = N / L - ((L - 1) / 2);
+			if(min < 0 || L > 100) {
+				sb.append(-1);
+				break;
+			}
+			
+			int sum = (min*2+L-1)*L/2;
+			
+			if(sum == N) {
+				for (int i = 0; i < L; i++) sb.append((min+i) + " ");
+                break;
+			}
+			L++;
+		}
+		System.out.println(sb);
+	}
+
+}
